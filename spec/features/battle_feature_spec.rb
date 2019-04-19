7cf638100f743135b017ebce32ece06c13f8de4e
sarpong <abasimisarpong@rocketmail.com>
2019-04-19 16:19:24 +0100

    feature test updated to incorporate dynamic user name

--- spec/features/battle_feature_spec.rb
+++ spec/features/battle_feature_spec.rb
@@ -8,7 +8,7 @@ end
 RSpec.feature 'Players HP' do
   scenario 'Check player2s hit points' do
     sign_in_and_play
-    expect(page).to have_content('Vegita HP: 8000')
+    expect(page).to have_content('Vegita 8000')
   end
 end
 
@@ -16,7 +16,7 @@ RSpec.feature 'Players HP' do
   scenario 'Confirms that you attacked player2' do
     sign_in_and_play
     click_button('Attack')
-    expect(page).to have_content('You attacked Vegita!')
+    expect(page).to have_content('Goku just attacked Vegita!')
   end
 end
 
@@ -24,6 +24,6 @@ RSpec.feature 'Players HP' do
   scenario 'Reduces player2s HP by 10' do
     sign_in_and_play
     click_button('Attack')
-    expect(page).to have_content('Vegita HP: 7990')
+    expect(page).to have_content('Vegita HP: 7900')
   end
 end
